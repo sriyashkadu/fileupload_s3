907a81d8e8dca43e8f9e3ebd0ae90307d106d554
sriyashkadu <58561811+sriyashkadu@users.noreply.github.com>
2020-04-20 19:34:56 +0530

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const ID = ' ';
+const SECRET = '';
+const BUCKET_NAME = '';
+
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
+
+const params = {
+    Bucket: BUCKET_NAME,
+    CreateBucketConfiguration: {
+        LocationConstraint: "us-west-1"
+    }
+};
+
+const uploadFile = (fileName) => {
+    // Read content from the file
+    const fileContent = fs.readFileSync(fileName);
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: '',
+        Key: '', // File name you want to save as in S3
+        Body: fileContent
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+};
+uploadFile(''); //specify the file name
